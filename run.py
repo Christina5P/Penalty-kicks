33b37678bc24386b3bb76ad66ea0b028320c7e2e
Christina_5P Åhman <christina.ahman@tv4.se>
2024-05-06 17:13:15 +0000

    start up py, add name input, add headings and links in read me

--- run.py
+++ run.py
@@ -1,3 +1,35 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+kicksdirection = ['lt', 'lb', 'mt', 'mb', 'rt', 'rb']
+lt = 'lefttop'
+lb = 'leftbottom'
+mt = 'middletop'
+mb = 'middlebottom'
+rt = 'righttop'
+rb = 'rightbottom'
+
+# Enter name and ask for instruction or move to game
+name = input('Hi Soccerplayer! Please enter your name: ')
+response = input(f'Thanks {name}, Do you want to see instructions? (y/n): ')
+
+if response.lower() == 'y':
+    print('instructions') 
+elif response.lower() == 'n':
+    print ("OK, click enter and let's start")
+else:
+    print ('Something went wrong, please answer (y/n):')
+
+
+
+
+
+totalPenalties=0
+userscore = 0
+computerscore = 0
+
+# Count goals from player and computer up to 6 shoots
+while totalPenalties < 6:
+    penaltyFor ()
+    penaltyAgainst()
+    penaltyScores()
+finalScore()    
