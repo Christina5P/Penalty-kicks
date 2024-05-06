89f425502a0d46c860d9d97549caac2d366fd4f8
Christina_5P Åhman <christina.ahman@tv4.se>
2024-05-06 20:47:19 +0000

    function penaltyAgainst to choose spot, a link in read me

--- README.md
+++ README.md
@@ -23,4 +23,7 @@
 https://trinket.io/python/909d6c5804- inspiration penalty definition
 https://www.giraffeacademy.com/programming-languages/python - tutoring of formatting
 https://www.w3schools.com/python - tutoring of formatting
+https://econowmics.com/python-penalty-kicks-simulator/ - function for penaltykicks
+https://www.101computing.net/penalty-shootout/ - tutoring video
+
 
--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import random
 
-kicksdirection = ['lt', 'lb', 'mt', 'mb', 'rt', 'rb']
+# List for direction you will kick  
+kicks_direction = ['lt', 'lb', 'mt', 'mb', 'rt', 'rb', 'out']
 lt = 'lefttop'
 lb = 'leftbottom'
 mt = 'middletop'
 mb = 'middlebottom'
 rt = 'righttop'
 rb = 'rightbottom'
+out = 'out'
 
 # Enter name and ask for instruction or move to game
 name = input('Hi Soccerplayer! Please enter your name: ')
@@ -15,14 +17,36 @@ response = input(f'Thanks {name}, Do you want to see instructions? (y/n): ')
 if response.lower() == 'y':
     print('instructions') 
 elif response.lower() == 'n':
-    print ("OK, click enter and let's start")
+    print("OK, we start!\n")
 else:
-    print ('Something went wrong, please answer (y/n):')
+    print('Something went wrong, please answer (y/n):')
 
+"""
+function for player to kick to any of 6 direction, player has choosen from.
+The goalkeeper will either stay in center or jump to left or right side
+to catch the ball.
+"""
 
+def penaltyAgainst():
 
+    # players choice of direction:
+    
+    print ('Choose spot from:\n lt=Left Top,\n lb=Left bottom,\n mt=Middle Top\n mb=Middle Bottom\n rt=Right Top\n rb=Right bottom')
+    userOption = input('Pick your spot:')   
 
+    # randomchoice for the goalkeeper:
+    goalkeeper = random.choice(kicks_direction)
+    print('Goalkeeper dives to', goalkeeper)
 
+    if goalkeeper == userOption:
+         print('Goalkeeper catched the ball!')
+    else:
+         print ('GOAL!')
+
+# call for the function
+penaltyAgainst()
+
+""" 
 totalPenalties=0
 userscore = 0
 computerscore = 0
@@ -32,4 +56,5 @@ while totalPenalties < 6:
     penaltyFor ()
     penaltyAgainst()
     penaltyScores()
-finalScore()    
+    finalScore()    
+""" 
